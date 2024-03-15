5f258a765a6abe740fc599cafe561be1615f8cf1
Ritik-Datey <ritikdatey2017@gmail.com>
2024-03-15 19:13:41 +0530

    learn new

---
+++ Java/src/stringRevision/StringRevision.java
@@ -0,0 +1,14 @@
+package stringRevision;
+
+public class StringRevision {
+
+	public static void main(String[] args) {
+
+		//String is a sequence of character that represent a text. It is present in Java.lang.String class and it is non primitive pre-deine class in java.
+		//we can create a string in two ways....1.By using String literals and 2. By using new keyword
+		
+		String name="Ritik Datey";
+
+	}
+
+}
