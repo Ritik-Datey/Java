58294e8409ba0f8afa8db03e2f9932fb32703846
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-04-10 13:06:22 +0000

    new

---
+++ Array/src/com/array/Reservse.java
@@ -0,0 +1,15 @@
+package com.array;
+
+public class Reservse {
+
+	public static void main(String[] args) {
+
+		int[] a={1,4,2,3,5,6,8,7,9};
+
+        for(int i=a.length;i>=0;i--)
+        {
+            System.out.println(a[i]);
+        }
+	}
+
+}
